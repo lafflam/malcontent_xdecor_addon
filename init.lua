e8488542e3666ca0d6be70ac6eed342d4035a751
malfal <lafflam@users.noreply.github.com>
2025-03-28 04:04:22 +0000

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------------
+-- todo: add translations
+--------------------------------------------------------------------------------
+
+malcontent_xdecor_addon = {}
+
+local modpath = core.get_modpath("malcontent_xdecor_addon")
+local version = "0.1.0"
+
+dofile(modpath .. "/src/storage_barrel.lua")
+
+print("[MOD] Loaded Malcontent X-Decor Addon version: " .. version)
